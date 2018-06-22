996cc2add5b47e217992d336b50389fd4b28a008
budparr <budparr@gmail.com>
2018-06-22 15:21:22 -0400

    clean up some unused scripts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,3 @@
 ---
 layout: null
 ---
-{% include scripts/jquery/dist/jquery.min.js %}
-
-
-// Mobile nav button/dropdown
-{% comment %}
-
-{% include scripts/links-new-window.js %}
-
-
-{% endcomment %}
